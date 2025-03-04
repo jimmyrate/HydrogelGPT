52ca6320cd16a7125ae10c943199a4f293f0f362
jimmyrate <57659929+jimmyrate@users.noreply.github.com>
2025-03-04 19:57:53 +0800

    Add files via upload

---
+++ code/path_parameters.py
@@ -0,0 +1,27 @@
+smiles_path = {'chembl25':'data/chembl25_unique_canon_Drd2Inactive_random30w.txt',
+            'gel_pos':'dataProcess/data/molecularGel_specific_neg/positive_clean.txt',
+            'gel_neg':'dataProcess/data/molecularGel_specific_neg/negative_clean.txt',
+            'gel_train_pos':'data/molecularGel/train_positive.txt',
+            'gel_train_neg':'data/molecularGel/train_negative.txt',
+            'gel_test_pos':'data/molecularGel/test_positive.txt',
+            'gel_test_neg':'data/molecularGel/test_negative.txt',
+            
+            'gel_pos_pep':'data/peptide&others/positive_peptides.txt',
+            'gel_pos_otr':'data/peptide&others/positive_others.txt',
+            }
+
+
+pre_selfies_path = {'chembl25':'data_pre/chembl25_unique_canon_Drd2Inactive_random30w.txt',
+            'gel_pos':'data_pre/molecularGel/positive_clean.txt',
+            'gel_neg':'data_pre/molecularGel/negative_clean.txt',
+            'gel_train_pos':'data_pre/molecularGel/train_positive.txt',
+            'gel_train_neg':'data_pre/molecularGel/train_negative.txt',
+            'gel_test_pos':'data_pre/molecularGel/test_positive.txt',
+            'gel_test_neg':'data_pre/molecularGel/test_negative.txt',
+            
+            'gel_pos_pep':'data_pre/peptide&others/positive_peptides.txt',
+            'gel_pos_otr':'data_pre/peptide&others/positive_others.txt',
+            }
+
+
+vocab_path = {'union_chembl25_gel':'./vocab/union_chembl25_gel.vocab'}
